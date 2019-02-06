f6c0a9696ee1e04bd02a0b48cf31afd44d53feee
Michael Plüss <m.pluess@stud.unibas.ch>
2019-02-06 15:57:16 +0100

    Working with different browsers is ABHORRENT!!! Added slide menu button

--- develop/wordpress/wp-content/themes/mobile/header.php
+++ develop/wordpress/wp-content/themes/mobile/header.php
@@ -24,6 +24,13 @@
 
     <?php if (wp_is_mobile()) { ?>
 	    <div class="container-mobile">
+
+            <div class="menu-container">
+                <div class="menu-filler"></div>
+                <div class="menu-button">
+                    <img style="width: 100%;" src="<?php echo get_template_directory_uri(); ?>/images/baseline_menu_black_48dp_2x.png" alt="Menu" />
+                </div>
+            </div>
     <?php } else { ?>
         <div class="container-desktop">
     <?php }; ?>
